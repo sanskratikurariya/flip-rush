28db4223f5377aa1ee924de7ce783f68066a7da3
sanskratikurariya <sanskratikurariya@gmail.com>
2024-03-31 18:31:09 +0530

    deploy

--- script.js
+++ script.js
@@ -1,40 +1,4 @@
 
-// document.addEventListener("DOMContentLoaded",function(){
-//     const music = document.getElementById("tick");
-    
-    
-//     // const playMusic = document.getElementById("play-btn");
-//     const pauseMusic =document.getElementById("pause-btn");
-
-//     pauseMUSIC.addEventListener('click',function)
-// e.target.classList.toggle("play-btn");
-//         music.play();
-
-//     });
-//     pauseMusic.addEventListener('click',function(e){
-//         music.pause();
-
-// document.addEventListener("DOMContentLoaded",function(){
-//     const music = document.getElementsByClassName(".tick")
-//   let playMusic = document.getElementsByClassName(".play-btn");
-//     let pauseMusic =document.getElementsByClassName(".pause-btn");
-//     pauseMusic.addEventListener('click',function(){
-//         music.pause();
-//         pauseMusic.style.display='none';
-//         playMusic.style.display='block';
-        
-
-// })
-// playMusic.addEventListener('click',function(){
-//     music.play();
-//     playMusic.style.display='none';
-//     pauseMusic.style.display='block';
-    
-
-// })
-
-
-// })
 
     const playbtn = document.querySelector(".play-btn");
     const music = document.querySelector(".tick");
@@ -56,12 +20,3 @@
 
  
  
-//  console.log(music.paused)
-//  if(music.paused){
-//      music.play();
-//      playbtn.classList.add('playing');
-//  }else{
-//      music.pause();
-//      playbtn.classList.remove('playing');
-//  }
-// }
